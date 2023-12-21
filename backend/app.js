551c3143130f63c58d11beb134c30bf7fdeb0607
melissamohellebi <melissamohellebi93@gmail.com>
2023-12-21 21:39:00 +0100

    application docker distribué

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
